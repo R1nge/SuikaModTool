4ed0a411d5eb68a5b6ab93ff8b4501282f8f3153
R1nge <r1ngegamedev@gmail.com>
2024-01-04 12:31:21 +0300

    Mod folder creation, config file creation

---
+++ SuikaModTool/GameConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace SuikaModTool;
+
+public partial class MainWindow
+{
+    public struct GameConfig : IComparable<GameConfig>
+    {
+        public string ModName;
+        public string ModIconPath;
+        public string ContainerImagePath;
+        public string[] SuikaSkinsImagesPaths;
+        public string[] SuikaIconsPaths;
+        public string[] SuikaAudioPaths;
+        public float[] SuikaDropChances;
+        public float TimeBeforeTimerTrigger;
+        public float TimerStartTime;
+        public string InGameBackgroundPath;
+
+        public int CompareTo(GameConfig other)
+        {
+            var modNameComparison = string.Compare(ModName, other.ModName, StringComparison.Ordinal);
+            if (modNameComparison != 0) return modNameComparison;
+            var modIconPathComparison = string.Compare(ModIconPath, other.ModIconPath, StringComparison.Ordinal);
+            if (modIconPathComparison != 0) return modIconPathComparison;
+            var containerImagePathComparison = string.Compare(ContainerImagePath, other.ContainerImagePath, StringComparison.Ordinal);
+            if (containerImagePathComparison != 0) return containerImagePathComparison;
+            var timeBeforeTimerTriggerComparison = TimeBeforeTimerTrigger.CompareTo(other.TimeBeforeTimerTrigger);
+            if (timeBeforeTimerTriggerComparison != 0) return timeBeforeTimerTriggerComparison;
+            var timerStartTimeComparison = TimerStartTime.CompareTo(other.TimerStartTime);
+            if (timerStartTimeComparison != 0) return timerStartTimeComparison;
+            return string.Compare(InGameBackgroundPath, other.InGameBackgroundPath, StringComparison.Ordinal);
+        }
+    }
+}
--- SuikaModTool/MainWindow.xaml.cs
+++ SuikaModTool/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace SuikaModTool;
 
 public partial class MainWindow : Window
 {
+    private GameConfig _configToSave = new();
+    private string _savePath = null;
+    
     public MainWindow()
     {
         InitializeComponent();
@@ -13,6 +19,74 @@ public partial class MainWindow : Window
     
     private void TitleChanged(object sender, TextChangedEventArgs args)
     {
+        _configToSave.ModName = ((TextBox) sender).Text;
         Debug.WriteLine($"Title has changed to: {((TextBox) sender).Text}");
+    }
+
+    private void SelectPreviewImageClicked(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            FileName = "Preview Image",
+            DefaultExt = ".jpg",
+            Filter = "Image Files(*.jpg;*.jpeg;*.bmp)|*.jpg;*.jpeg;.bmp;"
+        };
+        
+        bool? result = dialog.ShowDialog();
+        
+        if (result == true)
+        {
+            //TODO: append save file directory
+            var path = dialog.SafeFileName;//Path.GetDirectoryName(dialog.FileName)!;;
+            _configToSave.ModIconPath = path;
+            Debug.WriteLine($"File Path: {path}");
+        }
+    }
+
+    private void CreateModClicked(object sender, RoutedEventArgs e)
+    {
+        ShowSaveFileDialog();
+        if (Validate())
+        {
+            CreateMod();    
+        }
+    }
+    
+    private void ShowSaveFileDialog()
+    {
+        string dummyFileName = "Save Here";
+
+        SaveFileDialog sf = new SaveFileDialog
+        {
+            FileName = dummyFileName
+        };
+
+        if (sf.ShowDialog() == true)
+        {
+            string savePath = Path.GetDirectoryName(sf.FileName)!;
+            _savePath = savePath;
+            Debug.WriteLine($"SAVE PATH {_savePath}");
+        }
+    }
+
+    private bool Validate()
+    {
+        return true;
+    }
+
+    private void CreateMod()
+    {
+        var json = JsonConvert.SerializeObject(_configToSave);
+
+        var fullSavePath = Path.Combine(_savePath, _configToSave.ModName);
+        if (!Directory.Exists(fullSavePath))
+        {
+            Directory.CreateDirectory(fullSavePath);
+        }
+
+        var fullJsonPath = Path.Combine(fullSavePath, "config.json");
+        
+        File.WriteAllText(fullJsonPath, json);
+        Debug.WriteLine("Mod has been created");
     }
 }
