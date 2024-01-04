920ddd628dada5446f0e1b1821fa428b12aecb39
R1nge <r1ngegamedev@gmail.com>
2024-01-04 09:03:48 +0300

    Init

---
+++ SuikaModTool/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SuikaModTool;
+
+public partial class App : Application
+{
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                services.AddSingleton<MainWindow>();
+            }).Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
